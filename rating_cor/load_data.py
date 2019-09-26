614cacbd92526e3b70a492cf0a45a8d5afb73bb8
jedikarix <karolchecinski@wp.pl>
2019-09-26 11:58:50 +0200

    Fix load_data script

--- rating_cor/load_data.py
+++ rating_cor/load_data.py
@@ -3,4 +3,4 @@ import pandas as pd
 import os
 os.chdir('WhyR2019_Czadowe_Chlopaki')
 places_df = pd.read_csv('data/places.csv')
-pop_times = pd.read_csv('data/popular_times_1.csv').concat(pd.read_csv('data/popular_times_2.csv'))
+pop_times = pd.concat([pd.read_csv('data/popular_times_1.csv'), pd.read_csv('data/popular_times_2.csv')])
