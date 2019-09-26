bea728bfee85c9ece034dfe0adf970a64b85db84
jedikarix <karolchecinski@wp.pl>
2019-09-26 13:43:00 +0200

    Add get_places_pop

--- rating_cor/load_data.py
+++ rating_cor/load_data.py
@@ -1,6 +1,8 @@
 from rating_cor.type_dataframe import get_df_by_type
 import pandas as pd
 import os
+
 os.chdir('WhyR2019_Czadowe_Chlopaki')
 places_df = pd.read_csv('data/places.csv')
-pop_times = pd.concat([pd.read_csv('data/popular_times_1.csv'), pd.read_csv('data/popular_times_2.csv')])
+pop_times_df = pd.concat([pd.read_csv('data/popular_times_1.csv'), pd.read_csv('data/popular_times_2.csv')])
+
--- rating_cor/type_dataframe.py
+++ rating_cor/type_dataframe.py
@@ -10,3 +10,8 @@ def get_pop_by_time(pop_times_df, hour, day):
     return pop_times_by_time
 
 
+def get_places_pop(places_df, pop_times_df, hour, day):
+    pop_by_time = get_pop_by_time(pop_times_df, hour, day)
+    pop_by_time = pop_by_time[['place_id', 'occupancy_index']].groupby(['place_id']).mean()
+    return pop_by_time.join(places_df.set_index('place_id'))
+
